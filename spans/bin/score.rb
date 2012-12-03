3d921649ae0db83dc895e8623069b895952895a0
david.batelu <david.batelu@acquia.com>
2012-12-03 16:42:38 -0500

    Score.rb more stuff added to it

--- spans/bin/score.rb
+++ spans/bin/score.rb
@@ -4,16 +4,9 @@ require 'thor'
 
 default_date = 20121116
 
-path = File.expand_path("..", __FILE__) + '/aq/lib/aq/hosting/servers'
+#path = File.expand_path("..", __FILE__) + '/aq/lib/aq/hosting/servers'
 vitalstrings_path = File.expand_path("../../../", __FILE__) + "1CLICK/vitalstringscode/out/1C2-E-TEST/#{default_date}/output/STANFORD"
-
-def span(qid, vs)
-	out = exec("spans.pl -a -s #{qid} #{vs}")
-	out.split('\n').each do |line|
-		sum, st, ed, len = line.split(' ')
-
-
-
+summary_path =  File.expand_path("../../", __FILE__) + "data/summarydocuments/"
 
 class Score < Thor
 
@@ -21,7 +14,36 @@ class Score < Thor
 		str.include?("/")
 	end
 
+	def span(qid, vs)
+		out = exec("spans.pl -a -s #{qid} #{vs}")
+
+		# Vital strings that are not present in any summary
+		if out.empty?
+			@vs_dont_match[qid] ||= {}
+			@vs_dont_match[qid][vs] = {}
+		end
+
+		# Vital strings that match some summaries
+		out.split('\n').each do |line|
+			sum, st, ed, len = line.split(' ')
+			@vs_match[qid] ||= {}
+			@vs_match[qid][vs] ||= {}
+			@vs_match[qid][vs][sum] ||= {}
+			@vs_match[qid][vs][sum][all] ||= []
+			if !@vs_match[qid][vs][sum][smallest].nil? && @vs_match[qid][vs][sum][smallest].last > len.to_i
+				@vs_match[qid][vs][sum][smallest] = [st.to_i, len.to_i]
+			end
+			@vs_match[qid][vs][sum][all] << [[st.to_i, len.to_i]]
+		end
+
+		# Summary coverage
+		
+	end
+
 	@global_hash = {}
+	@vs_match = {}
+	@vs_dont_match = {}
+	@sum_cover = {}
 	DIR.glob(vitalstrings_path).each do |full_file_path|
 		# 1C2-E-TEST-0001.out
 		full_file_name = full_file_path.split("/").last
@@ -47,9 +69,6 @@ class Score < Thor
 		end
 	end
 
-	vs_match = {}
-	vs_dont_match = {}
-	doc_dont_match = {}
 
 	@global_hash.each do |qid, query|
 		query.each do |nid, nugget|
