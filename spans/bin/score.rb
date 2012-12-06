2dd5aa9e60439c159af76efdb7e590cde6c1319c
David Batelu <davidbatelu@gmail.com>
2012-12-06 16:48:49 -0500

    Deleting unecessary files

--- spans/bin/score.rb
+++ spans/bin/score.rb
@@ -1,21 +1,26 @@
 #!/usr/bin/ruby
 
-require 'thor'
+#require 'thor'
 
-default_date = 20121116
 
-#path = File.expand_path("..", __FILE__) + '/aq/lib/aq/hosting/servers'
-vitalstrings_path = File.expand_path("../../../", __FILE__) + "1CLICK/vitalstringscode/out/1C2-E-TEST/#{default_date}/output/STANFORD"
-summary_path =  File.expand_path("../../", __FILE__) + "data/summarydocuments/"
 
-class Score < Thor
+#class Score < Thor
+#class Score
+
+#default_date = 20121116
+default_date = 'processed'
+summary_path =  File.expand_path("../../", __FILE__) + "/data/summarydocuments/"
+vitalstrings_path = File.expand_path("../../../", __FILE__) + "/1CLICK/vitalstringscode/out/1C2-E-TEST/#{default_date}/output/STANFORD"
+spans_path = File.expand_path("../", __FILE__) + "/spans.pl"
 
 	def vitalstring?(str)
 		str.include?("/")
 	end
 
-	def span(qid, vs)
-		out = exec("spans.pl -a -s #{qid} #{vs}")
+	def spans(qid, vs)
+		puts "processing #{qid} - #{vs}"
+		vital_str = vs.last.join(' ')
+		out = `./spans.pl -a -s #{qid} #{vital_str}`
 
 		# Vital strings that are not present in any summary
 		if out.empty?
@@ -29,16 +34,16 @@ class Score < Thor
 			@vs_match[qid] ||= {}
 			@vs_match[qid][vs] ||= {}
 			@vs_match[qid][vs][sum] ||= {}
-			@vs_match[qid][vs][sum][all] ||= []
+			@vs_match[qid][vs][sum]['all'] ||= []
 
 			@sum_cover[qid] ||= {}
 			@sum_cover[qid][sum] ||= []
 
-			if !@vs_match[qid][vs][sum][smallest].nil? && @vs_match[qid][vs][sum][smallest].last > len.to_i
-				@vs_match[qid][vs][sum][smallest] = [st.to_i, len.to_i]
+			if !@vs_match[qid][vs][sum]['smallest'].nil? && @vs_match[qid][vs][sum]['smallest'].last > len.to_i
+				@vs_match[qid][vs][sum]['smallest'] = [st.to_i, len.to_i]
 			end
-			@vs_match[qid][vs][sum][all] << [[st.to_i, len.to_i]]
-			@sum_cover << [[st.to_i, len.to_i]]
+			@vs_match[qid][vs][sum]['all'] << [[st.to_i, len.to_i]]
+			@sum_cover[qid][sum] << [[st.to_i, len.to_i]]
 		end
 
 	end
@@ -47,7 +52,9 @@ class Score < Thor
 	@vs_match = {}
 	@vs_dont_match = {}
 	@sum_cover = {}
-	DIR.glob(vitalstrings_path).each do |full_file_path|
+	puts vitalstrings_path
+	Dir.glob("#{vitalstrings_path}/*").each do |full_file_path|
+		puts "here"
 		# 1C2-E-TEST-0001.out
 		full_file_name = full_file_path.split("/").last
 
@@ -59,15 +66,17 @@ class Score < Thor
 		query_id = full_file_name.split("-").last.split(".").first
 
 		@global_hash[query_id] = {}
-		open(file).readlines.each do |line|
+		open(full_file_path).readlines.each do |line|
 			line_array = line.split
+			puts line_array.inspect
 			length = line_array.length
 			# vital strings contain / in the first column
 			if vitalstring?(line_array.first)
+				puts "here oh oh"
 				vitalstring_id = line_array.first.split('-').last
 				vitalstring_id_1, vitalstring_id_2 = vitalstring_id.split("/")
 				@global_hash[query_id][vitalstring_id_1] ||= {}
-				@global_hash[query_id][vitalstring_id_1][vitalstring_id_2] = line_arry[1..length].inject([]) {|obj,ell| obj << ell unless ell.grep(/^DEP/);obj}
+				@global_hash[query_id][vitalstring_id_1][vitalstring_id_2] = line_array[1..length].inject([]) {|obj,ell| obj << ell unless ell[/^DEP/];obj}
 			end
 		end
 	end
@@ -81,4 +90,5 @@ class Score < Thor
 		end
 	end
 
-end
+puts @vs_match.inspect
+#end
